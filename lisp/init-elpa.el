659034eded058be036e13677ebe04ee473705f3d
tangyongqiang <tangyongqiang@bilibili.com>
2018-01-04 16:15:19 +0800

    update elpa

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -26,7 +26,8 @@
 
 ;; We include the org repository for completeness, but don't normally
 ;; use it.
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+;(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("popkit" . "http://elpa.popkit.org/packages/"))
 
 
 
